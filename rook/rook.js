6726c418ded70289558b0fe3f9a27ba77e36946c
Scott Currie <scurrie@spu.edu>
2014-06-03 22:20:32 -0700

    Add Angular test - Rook Score Tracker.

---
+++ rook/rook.js
@@ -0,0 +1,29 @@
+﻿angular.module('rook', ['ui.bootstrap']);
+var RookGame = function ($scope) {
+
+  $scope.players = [
+      { name: "John" },
+      { name: "Paul" },
+      { name: "George" },
+      { name: "Ringo" }
+    ];
+  $scope.suits = [{name: "Red"}, {name: "Yellow"}, {name: "Black"}, {name: "Green"}];
+  $scope.hands = [
+      { bid: 150, trump: "Red", pointsInKitty: "Yes", bidder: "George", team1Sum: 0, team2Sum: 0, class: ""},
+      { bid: 135, trump: "Yellow", pointsInKitty: "No", bidder: "Ringo", team1Sum: 160, team2Sum: 40, class: ""}
+    ];
+  $scope.curHand = $scope.hands[0];
+  
+  $scope.setCurrentHand = function(hand){
+    $scope.curHand.class = "";
+    $scope.curHand = hand;
+    $scope.curHand.class = "warning";
+  };
+
+  $scope.newHand = function() {
+      $scope.hands.push({ bid: "", trump: "", pointsInKitty: "", bidder: "", team1Sum: 0, team2Sum: 0, class: ""});
+      var hand = $scope.hands[$scope.hands.length-1];
+      $scope.setCurrentHand(hand);
+  };
+  $scope.setCurrentHand($scope.hands[0]);
+};
