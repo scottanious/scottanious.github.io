f4af9b83177e4b1407c69ef8f8de663e9d66aa6d
Scott Currie <scurrie@spu.edu>
2014-06-05 10:30:57 -0700

    Add local storage functionality to keep state.

--- rook/rook.js
+++ rook/rook.js
@@ -1,35 +1,57 @@
-﻿angular.module('rook', ['ui.bootstrap']);
-var RookGame = function ($scope) {
+﻿angular.module('rook', ['ui.bootstrap', 'LocalStorageModule']);
+var RookGame = function ($scope, localStorageService) {
 
-    $scope.players = [
+    var storedHands = localStorageService.get('hands');
+    var storedPlayers = localStorageService.get('players');
+    var storedNothing = localStorageService.get('foo');
+
+    if (storedPlayers) {
+        $scope.players = storedPlayers;
+    }
+    else {
+        $scope.players = [
         { name: "John" },
         { name: "Paul" },
         { name: "George" },
         { name: "Ringo" }
-    ];
-    $scope.suits = [{name: "Red"}, {name: "Yellow"}, {name: "Black"}, {name: "Green"}];
-    $scope.hands = [
-        { bid: 0, trump: "", pointsInKitty: "", bidder: "", team1Score: 0, team2Score: 0, team1Sum: 0, team2Sum: 0, opposingScore: 0, class: "" },
-    ];
+        ];
+    }
+
+    $scope.suits = [{ name: "Red" }, { name: "Yellow" }, { name: "Black" }, { name: "Green" }];
+
+    if (storedHands) {
+        $scope.hands = storedHands;
+    } else {
+        $scope.hands = [
+            { bid: 0, trump: "", pointsInKitty: "", bidder: "", team1Score: 0, team2Score: 0, team1Sum: 0, team2Sum: 0, opposingScore: 0, class: "" },
+        ];
+    }
+
     $scope.curHand = $scope.hands[0];
-  
-    $scope.setCurrentHand = function(hand){
-    $scope.curHand.class = "";
-    $scope.curHand = hand;
-    $scope.curHand.class = "warning";
+
+    $scope.setCurrentHand = function (hand) {
+        $scope.curHand.class = "";
+        $scope.curHand = hand;
+        $scope.curHand.class = "warning";
     };
 
-    $scope.newHand = function() {
-        $scope.hands.push({ bid: "", trump: "", pointsInKitty: "", bidder: "", team1Sum: 0, team2Sum: 0, class: ""});
-        var hand = $scope.hands[$scope.hands.length-1];
+    $scope.newHand = function () {
+        $scope.hands.push({ bid: "", trump: "", pointsInKitty: "", bidder: "", team1Sum: 0, team2Sum: 0, class: "" });
+        var hand = $scope.hands[$scope.hands.length - 1];
         $scope.setCurrentHand(hand);
     };
-
-    $scope.recalculateScores = function(){
+    $scope.newGame = function () {
+        $scope.hands = [
+             { bid: 0, trump: "", pointsInKitty: "", bidder: "", team1Score: 0, team2Score: 0, team1Sum: 0, team2Sum: 0, opposingScore: 0, class: "" },
+        ];
+        $scope.setCurrentHand($scope.hands[0]);
+        localStorageService.clearAll();
+    };
+    $scope.recalculateScores = function () {
         var h = $scope.curHand;
         var team1WonBid = true;
         if (h.bidder === $scope.players[0].name || h.bidder === $scope.players[2].name) { team1WonBid = true; }
-        else { team1WonBid = false;}
+        else { team1WonBid = false; }
         if (team1WonBid) {
             h.team2Score = h.opposingScore;
             if (h.opposingScore > 200 - h.bid) { h.team1Score = h.bid * -1; }
@@ -41,8 +63,8 @@ var RookGame = function ($scope) {
         }
         $scope.recalculateSums();
     };
-  
-    $scope.recalculateSums = function() {
+
+    $scope.recalculateSums = function () {
         for (i = 0; i < $scope.hands.length; i++) {
             var previousSumTeam1 = 0;
             var previousSumTeam2 = 0;
@@ -53,6 +75,8 @@ var RookGame = function ($scope) {
             $scope.hands[i].team1Sum = parseInt(previousSumTeam1) + parseInt($scope.hands[i].team1Score);
             $scope.hands[i].team2Sum = parseInt(previousSumTeam2) + parseInt($scope.hands[i].team2Score);
         }
+        localStorageService.set('hands', JSON.stringify($scope.hands));
+        localStorageService.set('players', JSON.stringify($scope.players));
     }
 
     $scope.setCurrentHand($scope.hands[0]);
